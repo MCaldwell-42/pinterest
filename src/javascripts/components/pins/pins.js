15799c2a13c251b44b0b608d4e7a9ec97615a808
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-05-14 20:58:08 -0500

    pins are showing

--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -1,9 +1,28 @@
 import pinsData from '../../helpers/data/pinsData';
+import util from '../../helpers/util';
+
+const bindEvents = () => {
+  document.getElementById('toBoardsBtn').addEventListener('click', () => {
+    document.getElementById('pins-page').classList.add('hide');
+    document.getElementById('boards-page').classList.remove('hide');
+  });
+};
+
+const writePins = (pins) => {
+  let domString = '';
+  domString += '<div class = "container row">';
+  pins.forEach((pin) => {
+    domString += `<img src="${pin.imageUrl}" alt="pin image">`;
+  });
+  domString += '</div>';
+  util.printToDom('pins-on-board', domString);
+};
 
 const initPins = (boardId) => {
+  bindEvents();
   pinsData.loadPinsForBoard(boardId)
     .then((pins) => {
-      console.error(pins);
+      writePins(pins);
     })
     .catch(err => console.error(err));
 };
